2448229aa560baa1cff09e5f09a3b9b3b790fbbd
ganga123321 <ganga123321p@gmail.com>
2026-03-25 16:39:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function buyNow() {
   alert("Redirecting to payment...");
-  window.location.href = "https://upi-payment-link-here";
+  window.location.href = ""upi://pay?pa=8866583829-7@ybl&pn=CHUTIYA&am=749&cu=INR";";
 }
 
 function addToCart() {
