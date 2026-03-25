b79f72a5656562862cdb9e081011e17aff6ee58b
ganga123321 <ganga123321p@gmail.com>
2026-03-25 16:13:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function buyNow() {
+  alert("Redirecting to payment...");
+  window.location.href = "https://upi-payment-link-here";
+}
+
+function addToCart() {
+  alert("Product added to cart!");
+}
